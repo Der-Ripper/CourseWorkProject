dd4bae3453c9193bafc3ac8ee2e66beae0e16486
Le-1v <51856403+Der-Ripper@users.noreply.github.com>
2024-06-07 10:23:39 +0300

    .

--- CourseWorkProject/settings.py
+++ CourseWorkProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#2q!h80to*%lm)lv@dyk=510see!tbo3whu538ti#o*0mqu+7g'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
